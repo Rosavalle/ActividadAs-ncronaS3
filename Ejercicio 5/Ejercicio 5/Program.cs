a0b797dd4fd3630aa1ab6669a2899cd5e6ca1b21
Rosavalle <125718156+Rosavalle@users.noreply.github.com>
2023-08-18 12:46:23 -0600

    Ejercicio 5

---
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numeros = { 4, 10, 45, -67, -7, 25, 89, 64 };
+
+Console.WriteLine("Arreglo original:");
+Console.WriteLine(string.Join(", ", numeros));
+
+Array.Reverse(numeros);
+Console.WriteLine("\nArreglo después de Reverse:");
+Console.WriteLine(string.Join(", ", numeros));
+
+Array.Sort(numeros);
+Console.WriteLine("\nArreglo después de Sort:");
+Console.WriteLine(string.Join(", ", numeros));
