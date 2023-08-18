d0ce9e2ccf987b1c1ac0a7a4946020dbed418746
Rosavalle <125718156+Rosavalle@users.noreply.github.com>
2023-08-18 12:33:44 -0600

    Ejercicio 1

---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Ingrese el tamaño del arreglo (min 5): ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+if (size < 5)
+{
+    Console.WriteLine("El mínimo del arreglo debe ser 5.");
+    return;
+}
+
+float[] arreglo = new float[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"Ingrese el valor {i + 1}: ");
+    arreglo[i] = Convert.ToSingle(Console.ReadLine());
+}
+Console.WriteLine("Valores ingresados:");
+foreach (float valor in arreglo)
+{
+    Console.WriteLine(valor);
+}
