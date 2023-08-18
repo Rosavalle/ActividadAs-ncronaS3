ab1ab92977410202e0a4647163e9c5d6c2734ffb
Rosavalle <125718156+Rosavalle@users.noreply.github.com>
2023-08-18 12:42:52 -0600

    Ejercicio 3

---
+++ Ejercicio 3/Ejercicio 3/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] palabras = { "Perro", "Casa", "Cebra", "Tortuga", "Burro", "Mar", "Dedo", "Tasa", "Nube", "Sol" };
+
+Console.WriteLine("Arreglo con posiciones inicializadas:");
+for (int i = 0; i < palabras.Length; i++)
+{
+    Console.WriteLine($"Posición {i}: {palabras[i]}");
+}
+Array.Sort(palabras);
+
+Console.WriteLine("\nArreglo ordenado:");
+foreach (string palabra in palabras)
+{
+    Console.WriteLine(palabra);
+}
