f6a63f47dcd3d5fedf6800bb5bace16ee1bcbbd6
Rosavalle <125718156+Rosavalle@users.noreply.github.com>
2023-08-18 12:44:26 -0600

    Ejercicio 4

---
+++ Ejercicio 4/Ejercicio 4/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] palabras = { "Burro", "Casa", "Cebra", "Dedo", "Mar", "Nube", "Perro", "Sol", "Tasa", "Tortuga" };
+
+Console.WriteLine("Arreglo ordenado:");
+foreach (string palabra in palabras)
+{
+    Console.WriteLine(palabra);
+}
+
+Console.WriteLine("BinarySearch");
+string[] Palabras_De_Buscar = { "Sol", "Dedo", "Casa" };
+foreach (string buscar in Palabras_De_Buscar)
+{
+    int La_Posicion = Array.BinarySearch(palabras, buscar);
+    if (La_Posicion >= 0)
+    {
+        Console.WriteLine($"La palabra {buscar} se encuentra en la posición {La_Posicion}");
+    }
+}
