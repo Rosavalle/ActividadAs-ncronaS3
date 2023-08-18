72df74017cc6a0d0ef11a3d6afab02c39b65d679
Rosavalle <125718156+Rosavalle@users.noreply.github.com>
2023-08-18 12:37:05 -0600

    Ejercicio 2

---
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] nombreCompleto = { "Rosa", "Griselda", "Valle" };
+Console.WriteLine("Valores en el arreglo:");
+foreach (string valor in nombreCompleto)
+{
+    Console.WriteLine(valor);
+}
+int longitudArreglo = nombreCompleto.Length;
+Console.WriteLine($"Tamaño del arreglo: {longitudArreglo}");
+
+Console.WriteLine("Tamaño del arreglo y sus valores:");
+for (int i = 0; i < longitudArreglo; i++)
+{
+    Console.WriteLine($"Posición {i}: {nombreCompleto[i]}");
+}
